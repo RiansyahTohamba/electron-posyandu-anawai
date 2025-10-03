65f4b7daa7b40c12a8cc06dc0767c1325e846dd0
RiansyahTohamba <mriansyah93@gmail.com>
2025-10-04 06:45:18 +0800

    add more chart

--- old-version/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron");
-
-contextBridge.exposeInMainWorld("api", {
-  getBabies: () => ipcRenderer.invoke("get-babies"),
-  addBaby: (baby) => ipcRenderer.invoke("add-baby", baby),
-  updateBaby: (baby) => ipcRenderer.invoke("update-baby", baby),
-  deleteBaby: (id) => ipcRenderer.invoke("delete-baby", id),
-});
